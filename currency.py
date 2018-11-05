fcd1d96f58a9df0548ecfdac100ce3a92b23448a
Peter Oas <oas.peter@gmail.com>
2018-11-05 14:05:50 -0500

    Step One Complete

---
+++ currency.py
@@ -0,0 +1,6 @@
+
+# rates = [("USD", "EUR", 0.88), ("USD", "EUR", 113.18) ("USD", "CAD", 1.31)]
+
+def convert(form, to, value):
+    if form == to: 
+        return value
---
+++ test_currency.py
@@ -0,0 +1,4 @@
+from currency import convert
+
+def test_convert():
+    assert convert("CAD", "CAD", 1) == 1
