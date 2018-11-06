f35b7c352f37f9d5194f200b9a53e66f453ea222
Peter Oas <oas.peter@gmail.com>
2018-11-05 21:11:04 -0500

    Step 2 & 3 Complete

--- currency.py
+++ currency.py
@@ -1,6 +1,10 @@
-
-# rates = [("USD", "EUR", 0.88), ("USD", "EUR", 113.18) ("USD", "CAD", 1.31)]
-
-def convert(form, to, value):
-    if form == to: 
+def convert(rates, value, current, to):
+    if current == to:
+        value = 1
         return value
+    for x in rates:
+        if current == x[1] and to == x[2]:
+            return value * x[0]
+    for x in rates:
+        if current == x[2] and to == x[1]:
+            return value / x[0]
--- test_currency.py
+++ test_currency.py
@@ -1,4 +1,10 @@
 from currency import convert
 
-def test_convert():
-    assert convert("CAD", "CAD", 1) == 1
+# def test_convert_when_same():
+#     assert convert([], 1, "USD", "USD") == 1
+
+def test_convert_when_forward():
+    assert convert([(0.74, "USD", "EUR")], 1, "USD", "EUR") == 0.74
+
+def test_convert_when_backward():
+    assert round(convert([(0.74, "USD", "EUR")], 1, "EUR", "USD"), 2) == 1.35
